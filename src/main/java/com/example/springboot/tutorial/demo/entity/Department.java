7b760100b3f1704313d04ceda73d1f2cb3ca1f69
Boat <Boat_loading@hotmail.com>
2022-08-18 00:19:18 +0700

    Inital commit on master

---
+++ src/main/java/com/example/springboot/tutorial/demo/entity/Department.java
@@ -0,0 +1,35 @@
+package com.example.springboot.tutorial.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data //lombok getter setter toString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+
+    @NotBlank(message = "Please add Department Name")
+    @Length(max = 5, min = 1)
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+
+
+}
---
+++ src/main/java/com/example/springboot/tutorial/demo/service/DepartmentService.java
@@ -0,0 +1,20 @@
+package com.example.springboot.tutorial.demo.service;
+
+import com.example.springboot.tutorial.demo.entity.Department;
+import com.example.springboot.tutorial.demo.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
+}
